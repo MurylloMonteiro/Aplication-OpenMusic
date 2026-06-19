0bf4015fccc8582a49c3dc11270335e0378fbcee
MurylloMonteiro <muryllomonteiro209@gmail.com>
2026-06-19 09:30:38 -0300

    Reafactor: Refact carouselMusic and carouselChannel

--- src/shared/CarouselChannels/Carousel.tsx
+++ src/shared/CarouselChannels/Carousel.tsx
@@ -18,17 +18,17 @@ export function CarouselChannel({search}: {search:string}) {
     },[])
 
     return (
-        <div className="mx-4 py-1" >
-            <Carousel className=" p-0">
+        <div className=" py-1" >
+            <Carousel className=" p-1">
                 <CardHeader>
                     <h1 className="pl-2 font-bold text-gray-100" >{search}</h1>
                 </CardHeader>
-                <CarouselContent className="px-3">
+                <CarouselContent className="px-5">
                     {musicArry?.map((i: any, index) => (
-                        <CarouselItem key={index} className="flex flex-col items-center basis-[130px] p-1 m-1 sm:basis-[15%]">
-                            <Card className="overflow-hidden rounded-full p-0   ">
+                        <CarouselItem key={index} className="flex flex-col items-center basis-[130px] p-1 m-1 sm:basis-[200px]">
+                            <Card className="overflow-hidden shadow-sm rounded-full p-0   ">
                                 <CardContent className="flex flex-col p-0 gap-0 overflow-hidden">
-                                    <img className="w-full h-[120px]  object-cover sm:h-[30%]" src={i.thumbnails[1]} alt="" />
+                                    <img className="w-full h-[120px]  object-cover sm:h-[200px]" src={i.thumbnails[1]} alt="" />
                                 
                                 </CardContent>
                             </Card>
--- src/shared/CarouselMusics/Carousel.tsx
+++ src/shared/CarouselMusics/Carousel.tsx
@@ -18,19 +18,18 @@ export function CarouselMusic({search}: {search:string}) {
     },[])
 
     return (
-        <div className="mx-4 py-1" >
-            <Carousel className=" p-0">
+        <div className=" py-1" >
+            <Carousel className="p-1">
                 <CardHeader>
                     <h1 className="pl-2 font-bold text-gray-100" >{search}</h1>
                 </CardHeader>
-                <CarouselContent className="px-3">
+                <CarouselContent className="px-5">
                     {musicArry?.map((i: any, index) => (
                         <CarouselItem key={index} className=" basis-[35%] p-1 m-1 sm:basis-[15%]">
                             <Card className="bg-gray-950 shadow-2xl overflow-hidden p-0 min-h-[180px] sm:min-h-[250px] ">
                                 <CardContent className="flex flex-col p-0 gap-0 overflow-hidden">
                                     <img className="w-full h-[90px]  object-cover sm:h-[30%]" src={i.thumbnails[1]} alt="" />
 
-                                        
                                     <div className="p-1 flex flex-col px-2">
                                         <p className="text-[90%] mt-auto line-clamp-3 font-bold m-0.5">{i.title}</p>
                                         <p className="text-[80%]  text-gray-400 truncate">{i.channel}</p>
